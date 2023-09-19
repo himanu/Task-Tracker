5567cc09b488e64ff6891b0e9f114d8728e2c744
Himanshu <hy287719@gmail.com>
2023-09-19 22:52:08 +0530

    Updated middleware

--- server/middleware.js
+++ server/middleware.js
@@ -4,31 +4,31 @@ const client = new OAuth2Client(process.env.CLIENT_ID);
 const jwt = require("jsonwebtoken");
 
 function verifyIdToken(req, res, next) {
-  const tokenId = req?.body?.accessToken || "";
-  if (!tokenId) 
-    return res.status(401).send({
-        error: "Please send token id"
+    const tokenId = req?.headers?.authorization?.split(" ")?.[1] ?? ""
+    if (!tokenId)
+        return res.status(401).send({
+            error: "Please send token id"
+        })
+    client.verifyIdToken({
+        idToken: tokenId,
+        audience: process.env.CLIENT_ID
+    }).then(({ payload }) => {
+        console.log('Authentication successful ', payload);
+        req.auth = payload;
+        next();
+    }).catch((err) => {
+        console.log('Authentication failed ', err.message);
+        res.status(401).send({
+            error: 'Authentication Failed'
+        });
     })
-  client.verifyIdToken({
-      idToken: tokenId,
-      audience: process.env.CLIENT_ID
-  }).then(({payload}) => {
-      console.log('Authentication successful ',payload);
-      req.auth = payload;
-      next();
-  }).catch((err) => {
-      console.log('Authentication failed ',err.message);
-      res.status(401).send({
-          error: 'Authentication Failed'
-      });
-  })
 }
 
 const verifyJwtToken = async (req, res, next) => {
     try {
         /** read token from req */
         const token = req.headers.authorization?.split(" ")?.[1];
-        if (!token) 
+        if (!token)
             return res.status(401).send({
                 error: "Please send JWT token"
             })
@@ -50,7 +50,7 @@ const verifyJwtToken = async (req, res, next) => {
         req.user = user;
         /** forward the request to next middleware/controller */
         next();
-    } catch(err) {
+    } catch (err) {
         console.log("error ", err);
         next({
             status: 400,
@@ -58,4 +58,4 @@ const verifyJwtToken = async (req, res, next) => {
         })
     }
 };
-module.exports = {verifyIdToken, verifyJwtToken};
+module.exports = { verifyIdToken, verifyJwtToken };
